46b1b8c19b53915332ff03d1c580fdebcd9abd72
pabloma62 <pabloma62@gmail.com>
2021-10-09 22:41:54 +0200

    modified

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world with express!'))
+app.get('/', (req, res) => res.send('<h1>Polma was here :p</h1>'))
 
 app.listen(3000);
 console.log('Server on port 3000')
