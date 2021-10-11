2631e273ac0e4a4030662d03bcd3f9af120d53c7
pabloma62 <pabloma62@gmail.com>
2021-10-11 09:42:36 +0200

    texto modificado

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1>Polma was here :p</h1>'))
+app.get('/', (req, res) => res.send('<h1>Instancia virtual Ubuntu creada por Polma</h1>'))
 
-app.listen(3000);
-console.log('Server on port 3000')
+app.listen(80);
+console.log('Server on port 80')
 
