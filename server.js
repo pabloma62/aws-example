67a3579745cf383776b8c54f30c073b3da63f8d7
pabloma62 <pabloma62@gmail.com>
2021-10-11 09:51:06 +0200

    mure lokillo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1>Instancia virtual Ubuntu creada por Polma</h1>'))
+app.get('/', (req, res) => res.send('<h1>mure que pasa bro</h1>'))
 
 app.listen(3000);
 console.log('Server on port 3000')
