b403a9e16f6955e1785ae91d43a361b807c2cfb9
pabloma62 <pabloma62@gmail.com>
2021-10-11 09:48:19 +0200

    modificado canal

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/', (req, res) => res.send('<h1>Instancia virtual Ubuntu creada por Polma</h1>'))
 
-app.listen(80);
-console.log('Server on port 80')
+app.listen(3000);
+console.log('Server on port 3000')
 
